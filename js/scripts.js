93d1004b6047aac262ab3443cf1273617abda6f5
cohencha <74265514+cohencha@users.noreply.github.com>
2022-03-03 10:25:55 -0500

    yay

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY29oZW5jaGEiLCJhIjoiY2t6dW12Zm1mMDhnbzJvbXl1cmYxY3BpNyJ9.qTcZ7QT6P96dIYJjSniWVw'
-//
-// $.getJSON('./data/ferry-stops-updates.geojson', function(nycferrystops) {
-//   console.log(nycferrystops)
+
+$.getJSON('./data/ferry-stops-updates.geojson', function(nycferrystops) {
+  console.log(nycferrystops)
 
 var nycCenter = [-73.940820, 40.7724818]
 
@@ -12,32 +12,32 @@ var map = new mapboxgl.Map({
   zoom: 10
 });
 
-//   nycferrystops.features.forEach(function(nycferrystop) {
-//     var mapMarker = new mapboxgl.Marker({
-//       color: '#2596be'
-//     })
-//     .setLngLat(nycferrystop.geometry.coordinates)
-//     .addTo(map);
-//   })
-// })
+  $('#fly-to-wallst').on('click', function() {
+    // when this is clicked, let's fly the map to Pier 11
+    map.flyTo({
+      center: [-74.006567, 40.7035214],
+      zoom: 16
+    })
+  })
 
-$('#fly-to-wallst').on('click', function() {
-  // when this is clicked, let's fly the map to Pier 11
-  map.flyTo({
-    center: [-74.006567, 0.7035214],
-    zoom: 13
+  nycferrystops.features.forEach(function(nycferrystop) {
+    var mapMarker = new mapboxgl.Marker({
+      color: '#2596be'
+    })
+    .setLngLat(nycferrystop.geometry.coordinates)
+    .addTo(map);
   })
 })
-
-$('.menu-ui a').on('click', function() {
-    // For each filter link, get the 'data-filter' attribute value.
-    var filter = $(this).data('filter');
-    $(this).addClass('active').siblings().removeClass('active');
-    markers.setFilter(function(f) {
-        // If the data-filter attribute is set to "all", return
-        // all (true). Otherwise, filter on markers that have
-        // a value set to true based on the filter name.
-        return (filter === 'all') ? true : f.properties[filter] === true;
-    });
-    return false;
-})
+//
+// $('.menu-ui a').on('click', function() {
+//     // For each filter link, get the 'data-filter' attribute value.
+//     var filter = $(this).data('filter');
+//     $(this).addClass('active').siblings().removeClass('active');
+//     markers.setFilter(function(f) {
+//         // If the data-filter attribute is set to "all", return
+//         // all (true). Otherwise, filter on markers that have
+//         // a value set to true based on the filter name.
+//         return (filter === 'all') ? true : f.properties[filter] === true;
+//     });
+//     return false;
+// })
